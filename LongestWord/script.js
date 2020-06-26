70da4dcedb3123574ef8b0e516a4b25166e71c80
Rashad Murad <61832735+RashadM444@users.noreply.github.com>
2020-06-26 05:22:54 +0400

    challenge done

--- LongestWord/script.js
+++ LongestWord/script.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+function LongestWord(sen) { 
+    let array = sen.split(' ');
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    
+    let charArray= [];
+    for (let i = 0; i < array.length; i++) {
+        let charNum = 0;
+        for (let t = 0; t < array[i].length; t++) {
+            if (alphabet.indexOf(array[i][t]) > -1){
+                charNum += 1;
+            } else {
+                charNum +=0;
+            }
+        }
+        charArray.push(charNum);
+    }
+    
+
+    return array[charArray.indexOf(Math.max(...charArray))];
+  
+  }
+
+  console.log(LongestWord('Hel232lo world'));  
